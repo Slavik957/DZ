2d63bb42d44dbf668b90607484d7ae6f0f6cd651
Горбунов Вячеслав <sgorbunov957@gmail.com>
2023-06-28 00:32:59 +0300

    Выполнил дамашнее задание

---
+++ Задача 4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число N!: ");
+int n  = int.Parse(Console.ReadLine());
+for(int i = 2; i <= n; i += 2) {
+         Console.Write(i + " ");
+}
+ Console.WriteLine();
